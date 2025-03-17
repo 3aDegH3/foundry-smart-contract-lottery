436b3d2b79e1271796a5e41662e69913bf5836d7
sadegh <sadegh.computer.aducation@gmail.com>
2025-03-17 19:00:43 +0330

    setup the project

---
+++ src/Raffel.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+/**
+ * @title A snample Raffel contract
+ * @author Mohammad Sadeq Jafari
+ * @notice This contract is for creating a sample raffel
+ * @dev Implements Chainlink VRFv2.5
+ */
+
+contract Raffel{
+    
+}
+
