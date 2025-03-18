fdac68dd4c32941206cd50c627e64ce46e0cbb70
sadegh <sadegh.computer.aducation@gmail.com>
2025-03-18 22:43:47 +0330

    commit for install forge install smartcontractkit/chainlink-brownie-contracts --commit

--- src/Raffel.sol
+++ src/Raffel.sol
@@ -1,5 +1,28 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
+
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts@1.3.0/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
 
 /**
  * @title A snample Raffel contract
@@ -8,7 +31,61 @@ pragma solidity ^0.8.19;
  * @dev Implements Chainlink VRFv2.5
  */
 
-contract Raffel{
-    
-}
+contract Raffel {
+    /**  Errors */
+    error Raffel__SendMoreToEnterRaffel();
 
+    uint256 private immutable i_enteranceFee;
+    //  @dev the duration of the lottery in seconds
+    uint256 private immutable i_interval;
+    uint256 private s_lastTimeStamp;
+    address payable[] private s_players;
+
+    /**Events */
+
+    event RaffelEnter(address indexed player);
+
+    constructor(uint256 enteranceFee, uint256 interval) {
+        i_enteranceFee = enteranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
+    }
+
+    function enterRaffel() external payable {
+        // require(msg.value >= i_enteranceFee, "Not enough ETH sent!");
+        // require(msg.value>=i_enteranceFee,Raffel__SendMoreToEnterRaffel());
+        if (msg.value < i_enteranceFee) {
+            revert Raffel__SendMoreToEnterRaffel();
+        }
+        s_players.push(payable(msg.sender));
+        emit RaffelEnter(msg.sender);
+    }
+
+    function pickWinner() external {
+        if ((block.timestamp - s_lastTimeStamp) > i_interval) {
+            revert();
+        }
+
+        requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: s_keyHash,
+                subId: s_subscriptionId,
+                requestConfirmations: requestConfirmations,
+                callbackGasLimit: callbackGasLimit,
+                numWords: numWords,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            })
+        );
+    }
+
+    /**
+     * Getter Functions
+     */
+
+    function getEnteranceFee() public view returns (uint256) {
+        return i_enteranceFee;
+    }
+}
