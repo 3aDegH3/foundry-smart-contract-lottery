3e1624b19df7e6727bdf3ce83764608829095208
sadegh <sadegh.computer.aducation@gmail.com>
2025-03-19 00:00:44 +0330

    implement vrf-based random number generation in the raffel contract

--- src/Raffel.sol
+++ src/Raffel.sol
@@ -18,42 +18,53 @@
 // internal
 // private
 // view & pure functions
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {VRFConsumerBaseV2Plus} from "@chainlink/contracts@1.3.0/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 
 /**
- * @title A snample Raffel contract
+ * @title A sample Raffel contract
  * @author Mohammad Sadeq Jafari
  * @notice This contract is for creating a sample raffel
  * @dev Implements Chainlink VRFv2.5
  */
 
-contract Raffel {
+contract Raffel is VRFConsumerBaseV2Plus {
     /**  Errors */
     error Raffel__SendMoreToEnterRaffel();
 
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
     uint256 private immutable i_enteranceFee;
-    //  @dev the duration of the lottery in seconds
     uint256 private immutable i_interval;
+    bytes32 private immutable i_keyHash;
+    uint256 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
     uint256 private s_lastTimeStamp;
     address payable[] private s_players;
 
-    /**Events */
-
+    /** Events */
     event RaffelEnter(address indexed player);
 
-    constructor(uint256 enteranceFee, uint256 interval) {
+    constructor(
+        uint256 enteranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint256 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2Plus(vrfCoordinator) {
         i_enteranceFee = enteranceFee;
         i_interval = interval;
         s_lastTimeStamp = block.timestamp;
+        i_keyHash = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffel() external payable {
-        // require(msg.value >= i_enteranceFee, "Not enough ETH sent!");
-        // require(msg.value>=i_enteranceFee,Raffel__SendMoreToEnterRaffel());
         if (msg.value < i_enteranceFee) {
             revert Raffel__SendMoreToEnterRaffel();
         }
@@ -62,29 +73,33 @@ contract Raffel {
     }
 
     function pickWinner() external {
-        if ((block.timestamp - s_lastTimeStamp) > i_interval) {
-            revert();
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert("Time interval has not passed yet");
         }
 
-        requestId = s_vrfCoordinator.requestRandomWords(
-            VRFV2PlusClient.RandomWordsRequest({
-                keyHash: s_keyHash,
-                subId: s_subscriptionId,
-                requestConfirmations: requestConfirmations,
-                callbackGasLimit: callbackGasLimit,
-                numWords: numWords,
+        VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient
+            .RandomWordsRequest({
+                keyHash: i_keyHash,
+                subId: i_subscriptionId,
+                requestConfirmations: REQUEST_CONFIRMATIONS,
+                callbackGasLimit: i_callbackGasLimit,
+                numWords: NUM_WORDS,
                 extraArgs: VRFV2PlusClient._argsToBytes(
-                    // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
                     VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
                 )
-            })
-        );
+            });
+
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
     }
 
-    /**
-     * Getter Functions
-     */
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] calldata randomWords
+    ) internal override {
+        // Handle random words
+    }
 
+    /** Getter Functions */
     function getEnteranceFee() public view returns (uint256) {
         return i_enteranceFee;
     }
